15326dd64e3d3b9ac9362c8e05903332b160ed37
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2013-01-12 23:10:30 -0800

    recent advances of gesture interpreter

--- leap_gestures/include/leap_gestures/GestureInterpreter.hpp
+++ leap_gestures/include/leap_gestures/GestureInterpreter.hpp
@@ -3,6 +3,8 @@
 
 #include "ros/ros.h"
 #include "leap_msgs/Leap.h"
+#include "leap_msgs/ControlStatus.h"
+#include "leap_msgs/GripperControl.h"
 #include "boost/foreach.hpp"
 #include "tf/transform_datatypes.h"
 
@@ -17,23 +19,31 @@ public:
 		_nh(nh)
 	{
 		_handEnteredWorkspaceTimestamp = ros::Time(0);
+		resetControl();
 
 		double temp;
 
 		ros::NodeHandle nhPriv("~");
 
-		nhPriv.param<double>("wait_before_hand_active", temp, 2.0);
+		nhPriv.param<double>("wait_before_hand_active", temp, 1.0);
 		_waitBeforeHandActive = ros::Duration(temp);
 
-		nhPriv.param<double>("workspace_origin/x", temp, 0);
-		_workspaceOrigin.setX(temp);
-		nhPriv.param<double>("workspace_origin/y", temp, 0);
-		_workspaceOrigin.setX(temp);
-		nhPriv.param<double>("workspace_origin/z", temp, 0.3);
-		_workspaceOrigin.setX(temp);
+		nhPriv.param<double>("controller_radius", _workspaceHandRadius, 0.1);
+		nhPriv.param<double>("controller_inner_radius", _workspaceHandInnerRadius, 0.03);
+		nhPriv.param<double>("gripper_control_threshold", _gripperControlThreshold, 0.06);
 
-		_subLeap = _nh.subscribe("leap", 10, &GestureInterpreter::leapCB, this);
-		_pubControl = _nh.advertise<geometry_msgs::PoseStamped>("leap/palm/pose", 10, true);
+		nhPriv.param<double>("workspace_origin/x", temp, 0);
+		_workspaceHand.setX(temp);
+		nhPriv.param<double>("workspace_origin/y", temp, 0);
+		_workspaceHand.setY(temp);
+		nhPriv.param<double>("workspace_origin/z", temp, 0.3);
+		_workspaceHand.setZ(temp);
+
+		_subLeap = _nh.subscribe("leap/data", 10, &GestureInterpreter::leapCB, this);
+		_pubControlHand = _nh.advertise<geometry_msgs::PoseStamped>("leap/palm/pose", 1, true);
+		_pubStatus = _nh.advertise<leap_msgs::ControlStatus>("leap/control_status", 10, true);
+		_pubControlGripper = _nh.advertise<leap_msgs::GripperControl>("leap/pr2_hand/gripper", 10, true);
+
 	}
 
 	void leapCB(const leap_msgs::Leap &msg)
@@ -42,38 +52,105 @@ public:
 
 		BOOST_FOREACH(const leap_msgs::Hand &hand, msg.hands)
 		{
+
 			if(inWorkspace(hand))
 			{
 				// this is the controlling hand
 
 				frameHasHand();
 
+
 				if(handActive())
 				{
-					// do control with hand
+					ROS_INFO("Frame has hand and active");
+					setActive(true);
 
+					///////////////////////////////////
+					// Extract Hand Position
+					///////////////////////////////////
 					tf::Point hand_point;
 					tf::pointMsgToTF(hand.pose.position, hand_point);
 
-					tf::Point relative_hand_point = hand_point - _workspaceOrigin;
+					tf::Point relative_hand_point = hand_point - _workspaceHand;
 
-					tf::pointTFToMsg(relative_hand_point, _controlPose.position);
-					_controlPose.orientation = hand.pose.orientation;
+					// the inner radius defines a  region where we want 0 movement
+					if (relative_hand_point.length() < _workspaceHandInnerRadius)
+					{
+						relative_hand_point.setValue(0,0,0);
+					}
+
+					tf::pointTFToMsg(relative_hand_point, _controlHandPose.position);
+					_controlHandPose.orientation = hand.pose.orientation;
+
+					///////////////////////////////////
+					// Check fingers for gripper control
+					///////////////////////////////////
+
+					BOOST_FOREACH(const int16_t & finger_id, hand.finger_ids)
+					{
+						leap_msgs::Finger finger;
+						BOOST_FOREACH(const leap_msgs::Finger &f, msg.fingers)
+						{
+							if(f.id == finger_id)
+							{
+								finger = f;
+								break;
+							}
+						}
+
+						tf::Pose poseFinger; // w_T_f
+						tf::poseMsgToTF(finger.pose, poseFinger);
+						tf::Pose poseHand; // w_T_h
+						tf::poseMsgToTF(hand.pose, poseHand);
+
+						//tf::Pose poseFingerInHandFrame = poseHand.inverse() * poseFinger;
+						tf::Pose poseFingerInHandFrame = poseFinger; // FIXME
+
+						ROS_INFO_STREAM(poseFingerInHandFrame.getOrigin().getX() << " " <<
+										poseFingerInHandFrame.getOrigin().getY() << " " <<
+										poseFingerInHandFrame.getOrigin().getZ() << ", " <<
+										_gripperControlThreshold);
+
+						ROS_INFO_STREAM("control value: " << fabs((poseFinger.getOrigin() - poseHand.getOrigin()).getY()));
+
+						if(fabs(poseFingerInHandFrame.getOrigin().getY()) > _gripperControlThreshold)
+						//if(fabs((poseFinger.getOrigin() - poseHand.getOrigin()).getY()) > _gripperControlThreshold)
+						{
+							ROS_INFO("Open");
+							_gripperControl.open = true;
+						}
+						else
+						{
+							ROS_INFO("Close");
+							_gripperControl.open = false;
+						}
+					}
+
 				}
 				else
 				{
-					resetControlPose();
+					ROS_INFO("Frame has hand but not active");
+					setActive(false);
+					resetControl();
 				}
+
+				return;
 			}
 		}
 
-		// no hand found
+		// no hand in workspace found
 		frameHasNoHand();
 	}
 
-	void resetControlPose()
+	void setActive(bool active)
 	{
-		_controlPose = geometry_msgs::Pose();
+		_workspaceHandActive = active;
+	}
+
+	void resetControl()
+	{
+		_controlHandPose = geometry_msgs::Pose();
+		_controlHandPose.orientation.w = 1;
 	}
 
 	void frameHasHand()
@@ -87,34 +164,57 @@ public:
 	void frameHasNoHand()
 	{
 		_handEnteredWorkspaceTimestamp = ros::Time(0);
-		resetControlPose();
+		resetControl();
+		setActive(false);
 	}
 
 	bool handActive()
 	{
-		return (!_handEnteredWorkspaceTimestamp.isZero()) &&
-				(ros::Time::now() - _handEnteredWorkspaceTimestamp) > _waitBeforeHandActive;
+		if (!_handEnteredWorkspaceTimestamp.isZero())
+		{
+			ros::Duration timeSinceHandInWorkspace =
+					ros::Time::now() - _handEnteredWorkspaceTimestamp;
+			if(timeSinceHandInWorkspace > _waitBeforeHandActive)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	bool inWorkspace(const leap_msgs::Hand &hand)
 	{
 		tf::Point hand_point;
 		tf::pointMsgToTF(hand.pose.position, hand_point);
-		return (hand_point - _workspaceOrigin).length() < 0.1;
+		ROS_INFO_STREAM((hand_point - _workspaceHand).length());
+		return (hand_point - _workspaceHand).length() < _workspaceHandRadius;
 	}
 
 	void run()
 	{
-		ros::Rate r(ros::Duration(0.01));
+		ros::Rate r(100);
 		while(ros::ok())
 		{
 			ros::spinOnce();
 
-			geometry_msgs::PoseStamped msg;
-			msg.header.frame_id = "leap_control_link";
-			msg.header.stamp = ros::Time::now();
-			msg.pose = _controlPose;
-			_pubControl.publish(msg);
+			{
+				geometry_msgs::PoseStamped msg;
+				msg.header.frame_id = "leap_control_link";
+				msg.header.stamp = ros::Time::now();
+				msg.pose = _controlHandPose;
+				_pubControlHand.publish(msg);
+			}
+
+			{
+				leap_msgs::ControlStatus msg;
+				msg.control_active = _workspaceHandActive;
+				_pubStatus.publish(msg);
+			}
+
+			if(_workspaceHandActive)
+			{
+				_pubControlGripper.publish(_gripperControl);
+			}
 
 			r.sleep();
 		}
@@ -124,14 +224,26 @@ private:
 	ros::NodeHandle _nh;
 
 	ros::Subscriber _subLeap;
-	ros::Publisher _pubControl;
+	ros::Publisher _pubControlHand;
+	ros::Publisher _pubControlGripper;
+	ros::Publisher _pubStatus;
 
-	tf::Point _workspaceOrigin;
+	tf::Point _workspaceHand;
+//	tf::Point _workspaceGripper;
+	double _workspaceHandRadius;
+	double _workspaceHandInnerRadius;
+//	double _workspaceGripperRadius;
+	bool _workspaceHandActive;
+//	bool _workspaceGripperActive;
+
+	geometry_msgs::Pose _controlHandPose; // pose sent to the controller. 0 if not active
+	leap_msgs::GripperControl _gripperControl;
+
+	double _gripperControlThreshold;
 
 	// zero if last frame had no hand in workspace
 	ros::Time _handEnteredWorkspaceTimestamp;
 
-	geometry_msgs::Pose _controlPose;
 
 	ros::Duration _waitBeforeHandActive;
 };
