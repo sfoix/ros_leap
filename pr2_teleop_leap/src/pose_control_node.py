80765619671f733287542d5adb7f4b6ab18a3594
Kai Franke <kai@kaifranke.com>
2013-01-12 23:19:02 -0800

    working commit, gripper_node added

--- pr2_teleop_leap/src/pose_control_node.py
+++ pr2_teleop_leap/src/pose_control_node.py
@@ -8,19 +8,19 @@ from geometry_msgs.msg import PoseStamped
 
 rospy.init_node('leap_pose_control')
 armr = simple_robot_control.Arm('r')
-multiplier = 1.0
+multiplier = 3.0
 
 def callback(msg):
-	rospy.loginfo("I received a message! w00t")
-	x = (msg.pose.position.x * multiplier) + 0.5
-	y = msg.pose.position.y * multiplier
-	z = msg.pose.position.z * multiplier
+	#ospy.loginfo("I received a message! w00t")
+	x = (msg.pose.position.x * multiplier) + 0.55
+	y = msg.pose.position.y * multiplier - 0.2
+	z = msg.pose.position.z * multiplier - 0.1
 	#armr = simple_robot_control.Arm('r')
-	armr.goToPose([x,y,z],[msg.pose.orientation.x,msg.pose.orientation.y,msg.pose.orientation.z,msg.pose.orientation.w ], 'torso_lift_link', 3.0,False)
+	armr.goToPose([x,y,z],[msg.pose.orientation.x,msg.pose.orientation.y,msg.pose.orientation.z,msg.pose.orientation.w ], 'torso_lift_link', 0.5,False)
 
 def PalmPose():
    #rospy.init_node('leap_pose_control')
-   rospy.Subscriber("/leap/palm/pose", PoseStamped, callback)
+   rospy.Subscriber("/leap/palm/pose", PoseStamped, callback, queue_size = 1)
    rospy.spin()
 
 if __name__ == '__main__':
